91d00a12e4f7b69d6ee75b0464b17f604f30fdaf
mohammad-biti <112351040+mohammad-biti@users.noreply.github.com>
2022-09-21 17:07:59 +0430

    Add files via upload

---
+++ 27/027-1/1.js
@@ -0,0 +1,4 @@
+let a =prompt("ghad ba hasb metr");
+let b =prompt("vazn");
+console.log(b/a**2);
+alert(b/a**2);
---
+++ 27/027-2/2.js
@@ -0,0 +1,4 @@
+let a =prompt('dama bar hasb far')
+let b =(a-32)*5/9
+alert(b)
+console.log(b)
